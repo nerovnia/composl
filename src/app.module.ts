77ac4061456e4e06dc908ee888f8b5cc3ba842b6
Volodymyr Nerovnia <nerv@i.ua>
2023-01-05 07:19:35 +0200

    Created modules:   - accounting   - auth   - company   - servcom   - servpay   - top-page

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { TopPageModule } from './top-page/top-page.module';
+import { AccountingModule } from './accounting/accounting.module';
+import { CompanyModule } from './company/company.module';
+import { ServcomModule } from './servcom/servcom.module';
+import { ServpayModule } from './servpay/servpay.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, TopPageModule, AccountingModule, CompanyModule, ServcomModule, ServpayModule],
   controllers: [AppController],
   providers: [AppService],
 })
